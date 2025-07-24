86d0dbf22a3b1ee1dd41154006bfa42d59dcb270
JuanLopez2004 <jlopeze22004@gmail.com>
2025-07-23 23:20:24 -0500

    feat: discordpy structure

---
+++ main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import logging
+import os   
+from dotenv import load_dotenv
+
+## Load environment variables from .env file
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+## Intent handling for message content and members
+handler = logging.FileHandler(filename='discord.log', encoding="utf8", mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+intents.presences = True
+
+## Prefix
+bot = commands.Bot(command_prefix='?', intents=intents)
+bot.remove_command("help")  
+
+## Logging 
+@bot.event 
+async def on_ready():
+    print(f"Acy is Ready.")
+
+## Test Command 
+
+@bot.command(name='test')
+async def test_command(ctx):                            
+    await ctx.send("hi chud")
+
+## Run the bot with the token and logging handler
+bot.run (token, log_handler=handler, log_level=logging.DEBUG)
